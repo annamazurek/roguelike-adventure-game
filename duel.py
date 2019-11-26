81cdf97db6385ea306d28b5d209cf0413794c89f
rlyyah <tmakowski1337@gmail.com>
2019-11-26 13:29:42 +0100

    create inicial functions

---
+++ duel.py
@@ -0,0 +1,42 @@
+def player_profile():
+    # return [playerATK, playerHP, and other stuff] 
+    pass
+
+
+def moster_profile():
+    # resturn [monsterATK, and stuff]
+    pass
+
+
+def update_health(amount):
+    pass
+
+
+def update_mana(amount):
+    pass
+
+
+def print_fight():
+    pass
+
+
+def duel_menu():
+
+    is_running = True
+    while is_running:
+
+        # inicialize fight
+
+        # create object of monster
+
+        # clear screen
+
+        # display fight screen
+
+        # input with player turn
+
+        # monster turn
+
+        # if one of them dead - Finish screen - Loot?
+
+        # back to the game
