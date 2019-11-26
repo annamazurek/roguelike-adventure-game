c28abbeb0368e5f084ff50190735e61cf0d40513
rlyyah <tmakowski1337@gmail.com>
2019-11-27 00:11:12 +0100

    add main connection to the duel, delete generating whole map

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import engine
 import ui
 import end_game
 import hero_info
+import duel
 
 PLAYER_ICON = '@'
 PLAYER_START_X = 3
@@ -32,12 +33,22 @@ def create_player():
     player["y"] = PLAYER_START_Y
     player["icon"] = PLAYER_ICON
     player["actual_stats"] = hero_info.actual_stats
+    player['hp'] = 50
+    player['dmg'] = 5
     return player
 
 
 def change_position(movement, player, board):
-    new_y = player['y'] + movement[0]
-    new_x = player['x'] + movement[1]
+
+    y_pos = 0
+    x_pos = 1
+
+    new_y = player['y'] + movement[y_pos]
+    new_x = player['x'] + movement[x_pos]
+    
+    if board[new_y][new_x] == 'S': 
+        duel.duel_menu(player)
+        # check if player is dead and move - alive->move dead->finish
     if board[new_y][new_x] == '#':
         pass
     else:
@@ -46,8 +57,10 @@ def change_position(movement, player, board):
     return player
 
 
+
 def main():
 
+    #inicialize before start
     clear_screen()
     player = create_player()
     board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
@@ -59,8 +72,8 @@ def main():
 
     is_running = True
     while is_running:
-        key = key_pressed()
-        board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
+        key = key_pressed().lower()
+        # board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
         player["actual_stats"] = hero_info.actual_stats
         hero_statistics = player["actual_stats"]
 
